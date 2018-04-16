25712bb20d5ccc81f1de30c6b90017d93e6f8023
wuyunshuang <941994590@qq.com>
2018-04-16 17:16:30 +0800

    添加包及文件

---
+++ src/main/java/com/wys/test/Test.java
@@ -0,0 +1,20 @@
+/*
+ * 文件名：Test.java
+ * 版权：Copyright by www.itqy.cn
+ * 描述：
+ * 修改人：Administrator
+ * 修改时间：2018-04-16
+ */
+
+package com.wys.test;
+
+public class Test
+{
+
+    public static void main(String[] args)
+    {
+        // TODO Auto-generated method stub
+
+    }
+
+}
